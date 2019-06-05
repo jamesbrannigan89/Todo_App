eadc43a5b43d6f17108a25905bf1a580eefc9bec
James Brannigan <jamesbrannigan89@gmail.com>
2019-06-05 22:48:56 +0100

    Test Commit
    Signed-off-by: James Brannigan <jamesbrannigan89@gmail.com>

---
+++ src/NotePad/Notepad.java
@@ -0,0 +1,18 @@
+package NotePad;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.*;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.swing.*;
+
+public class Notepad  extends  JFrame {
+
+
+
+
+}
